4d2cb95000f2b5b3879690f6a6d9d83effb97a1d
Maciej Biłas <maciej.bilas@gmail.com>
2023-04-14 12:34:14 +0200

    cli

--- hare.go
+++ hare.go
@@ -7,17 +7,34 @@ import (
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
+	app := &cli.App{
+		Name: "hare",
+		Flags: []cli.Flag{
+			&cli.StringFlag{Name: "addr", Usage: "Address to listen on", Value: "localhost:3200"},
+		},
+		Action: start,
+	}
+
+	_ = app.Run(os.Args)
+}
+
+func start(cCtx *cli.Context) error {
 	mux := chi.NewMux()
 
 	mux.Get("/headers", headersHandler)
 
-	err := http.ListenAndServe("localhost:3200", mux)
+	fmt.Printf("HARE starting. Listening on %s\n", cCtx.String("addr"))
+
+	err := http.ListenAndServe(cCtx.String("addr"), mux)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Error on shutdown: %s\n", err)
 	}
+
+	return nil
 }
 
 func headersHandler(w http.ResponseWriter, r *http.Request) {
